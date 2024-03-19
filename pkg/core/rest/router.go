e65162837b8ee8bfbab3d4a23208d973863b4f00
Femi Olowonefa <feminefa@gmail.com>
2024-03-19 13:27:54 -0400

    added cors

--- pkg/core/rest/router.go
+++ pkg/core/rest/router.go
@@ -39,9 +39,25 @@ func NewRestService(mainCtx *context.Context) *RestService {
 		ClientHandshakeChannel: clientVerificationc,
 	}
 }
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
 
+        c.Header("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
 func (p *RestService) Initialize() *gin.Engine {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/api/ping", func(c *gin.Context) {
 
 		// Send a response back
