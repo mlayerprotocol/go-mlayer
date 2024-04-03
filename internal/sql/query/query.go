4845cc233774d6f7093263d6e72a1c42121d791d
dephizee <hafiz227@gmail.com>
2024-04-03 14:26:38 +0100

    feat: block stats

--- internal/sql/query/query.go
+++ internal/sql/query/query.go
@@ -20,8 +20,13 @@ func GetOne[T any, U any](filter T, data *U) error {
 	return nil
 }
 
+func GetManyTx[T any](item T) *gorm.DB {
+	return db.Db.Where(&item)
+
+}
+
 func GetMany[T any, U any](item T, data *U) error {
-	err := db.Db.Where(&item).Find(data).Error
+	err := GetManyTx(item).Find(data).Error
 	if err != nil {
 		return err
 	}
@@ -32,6 +37,15 @@ func GetMany[T any, U any](item T, data *U) error {
 func GetWithIN[T any, U any, I any](item T, data *U, slice I) error {
 	err := db.Db.Find(data, slice).Error
 
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetManyGroupBy[T any, U any](item T, data *U, gb string) error {
+	err := db.Db.Where(&item).Select(fmt.Sprintf("topic_events.*, count(%s) as total", gb)).Group(gb).Find(data).Error
 	if err != nil {
 		return err
 	}
@@ -85,6 +99,62 @@ func SaveRecord[Model any](where Model, data Model, update bool, DB *gorm.DB) (m
 	}
 	return &data, result.RowsAffected > 0, nil
 }
+
+// func IncrementRecord[Model any](where Model, field string, DB *gorm.DB) (model *Model, created bool, err error) {
+// 	tx := DB
+// 	if DB == nil {
+// 		tx = db.Db.Begin()
+// 	}
+// 	// dataByte, err := encoder.MsgPackStruct(event.Payload)
+// 	if err != nil {
+// 		return nil, false, err
+// 	}
+// 	// authPayload := entities.ClientPayload{
+// 	// 	Data: entities.Authorization{},
+// 	// }
+// 	// copier.Copy(&authPayload, &event.Payload)
+// 	// data := models.AuthorizationEvent{
+// 	// 	Event: *event,
+// 	// }
+// 	var result *gorm.DB
+// 	var data Model
+// 	logger.Infof("111111::::: %s", where)
+// 	result = tx.Where(where).Find(data)
+// 	if result.Error != nil {
+// 		tx.Rollback()
+
+// 		return nil, false, result.Error
+// 	}
+// 	if result.RowsAffected == 0 {
+// 		logger.Infof("111111 TTTT ::::: %s", where)
+// 		result = tx.Where(where).FirstOrCreate(&data)
+// 	} else {
+// 		logger.Infof("111111 FFFF ::::: %s", where)
+// 		result = tx.Where(where).Update(field, fmt.Sprintf("%s + 1", field))
+
+// 	}
+// 	// if update {
+// 	// 	result = tx.Where(where).Assign(data).FirstOrCreate(&data)
+// 	// } else {
+// 	// 	result = tx.Where(where).FirstOrCreate(&data)
+// 	// }
+// 	logger.Infof("22222 ::::: %s", where)
+// 	if result.Error != nil {
+// 		tx.Rollback()
+
+// 		return nil, false, result.Error
+// 	}
+// 	logger.Infof("33333 ::::: %s", where)
+// 	if DB == nil {
+// 		if tx.Commit().Error != nil {
+// 			tx.Rollback()
+// 			return nil, false, tx.Error
+// 		}
+
+// 	}
+// 	logger.Infof("44444 ::::: %s", where)
+// 	return &data, result.RowsAffected > 0, nil
+// }
 
 func GetTableName(table any) string {
 	stmt := &gorm.Statement{DB: db.Db}
