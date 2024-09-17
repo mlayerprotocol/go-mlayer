5f0c2b4e5e67e28026630710c953de82c0db8101
Femi Olowonefa <feminefa@gmail.com>
2024-09-17 11:03:48 -0400

    fixed sync bug

--- pkg/core/sql/migration/migrations.go
+++ pkg/core/sql/migration/migrations.go
@@ -39,6 +39,7 @@ func init() {
 	AddMigration(DropOwnerColumnFromSubnetState, "2024-09-16 5:35PM") 
 	AddMigration(DropTopicIdColumnFromMessageState, "2024-09-16 5:12PM") 
 	AddMigration(DropAttachmentsColumnFromMessageState, "2024-09-16 5:42PM") 
+	AddMigration(DropAgentColumnFromSubnetState, "2024-09-16=7 10:42AM") 
 
 }
 
